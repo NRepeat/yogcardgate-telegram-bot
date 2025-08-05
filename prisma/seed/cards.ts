ffa71e9d0baabbb3cd50fc8409518c87b379ebb6
Nikita Nazarov <nnazarov55@gmail.com>
2025-08-05 17:14:17 +0000

    fix

--- prisma/seed/cards.ts
+++ prisma/seed/cards.ts
@@ -4,7 +4,7 @@ import {
   PaymentMethodEnum,
   PrismaClient,
   RoleEnum,
-} from 'generated/prisma';
+} from '@prisma/client';
 import * as dotenv from 'dotenv';
 import * as path from 'path';
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });

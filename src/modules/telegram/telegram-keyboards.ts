d3189089d4edf686b26ed31e83636d15520923d8
Nikita Nazarov <nnazarov55@gmail.com>
2025-09-05 14:38:23 +0000

    fix: set default value for isWorkGroup parameter in MenuFactory

--- src/modules/telegram/telegram-keyboards.ts
+++ src/modules/telegram/telegram-keyboards.ts
@@ -390,7 +390,7 @@ export class MenuFactory {
     request: FullRequestType,
     url: string,
     source?: Buffer<ArrayBufferLike>,
-    isWorkGroup?: boolean,
+    isWorkGroup = false,
   ): WorkMenu {
     return new WorkMenu(url, request, source, isWorkGroup);
   }

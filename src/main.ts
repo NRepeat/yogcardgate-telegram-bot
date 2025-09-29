71d8e007214aa82a60cf738c7c627befb1bbbeaa
Nikita Nazarov <nnazarov55@gmail.com>
2025-09-29 10:53:20 +0000

    feat: update main.ts to configure Telegram bot webhook and change default port to 3000 for improved integration

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { Telegraf } from 'telegraf';
+import { getBotToken } from 'nestjs-telegraf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT', 3001);
+  const port = configService.get<number>('PORT', 3000);
 
-  // const bot = app.get<Telegraf>(getBotToken());
-  // app.use(bot.webhookCallback('/telegram/webhook'));
+  const bot = app.get<Telegraf>(getBotToken());
+  app.use(bot.webhookCallback('/telegram/webhook'));
 
   app.enableCors();
 

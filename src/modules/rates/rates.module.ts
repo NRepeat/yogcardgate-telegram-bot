e42fdc89563967eca079bc50a89ab6cdf5ec6509
Nikita <95132082+NRepeat@users.noreply.github.com>
2026-03-05 12:31:47 +0200

    fix

--- src/modules/rates/rates.module.ts
+++ src/modules/rates/rates.module.ts
@@ -4,9 +4,15 @@ import { RatesService } from './rates.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { VendorModule } from '../vendor/vendor.module';
 import { UtilsModule } from '../utils/utils.module';
+import { ExternalApiModule } from '../external-api/external-api.module';
 
 @Module({
-  imports: [PrismaModule, VendorModule, forwardRef(() => UtilsModule)],
+  imports: [
+    PrismaModule,
+    VendorModule,
+    forwardRef(() => UtilsModule),
+    ExternalApiModule,
+  ],
   controllers: [],
   providers: [RatesRepository, RatesService],
   exports: [RatesRepository, RatesService],
